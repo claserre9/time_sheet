56e9e1e7a8d16f080136f6271cf4efa5d73117b6
Clifford Laserre <claserre9@gmail.com>
2021-02-01 13:52:49 -0800

    Additional setting for serving static files v6

--- time_sheet/settings.py
+++ time_sheet/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
-import dj_database_url
+
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
@@ -122,11 +122,3 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-# The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # . os.path.join(BASE_DIR, 'staticfiles')
-
-# The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
