10ae46b53077c45c9bd267533b240cd2037a87f3
Clifford Laserre <claserre9@gmail.com>
2021-02-01 09:04:00 -0800

    Register all models for administration

--- app_base/admin.py
+++ app_base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from app_base import models
+
+admin.site.register(models.Project)
+admin.site.register(models.Employee)
+admin.site.register(models.TimeSheet)
+admin.site.register(models.TimeSheetDetail)
