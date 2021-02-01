d251f1f18c46386ef67b86dbafb07f054ede6f2d
Clifford Laserre <claserre9@gmail.com>
2021-02-01 10:00:13 -0800

    Time Sheet Details Stacked inline in Time Sheet view

--- app_base/admin.py
+++ app_base/admin.py
@@ -2,8 +2,23 @@ from django.contrib import admin
 
 # Register your models here.
 from app_base import models
+from app_base.models import TimeSheet, TimeSheetDetail
+
+
+class TimeSheetDetailsInline(admin.TabularInline):
+    model = TimeSheetDetail
+
+
+class TimeSheetAdmin(admin.ModelAdmin):
+    inlines = [TimeSheetDetailsInline]
+
+    class Meta:
+        model = TimeSheet
+
+
+admin.site.register(models.TimeSheet, TimeSheetAdmin)
 
 admin.site.register(models.Project)
 admin.site.register(models.Employee)
-admin.site.register(models.TimeSheet)
-admin.site.register(models.TimeSheetDetail)
+
+# admin.site.register(models.TimeSheetDetail)
